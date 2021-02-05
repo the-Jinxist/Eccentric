da28b51d48b421bb471ef8cbed275af2961c7373
the-Jinxist <nerosilva522@gmail.com>
2021-02-05 20:22:00 +0100

    Updated API repo

--- lib/datasources/api/api_utils.dart
+++ lib/datasources/api/api_utils.dart
@@ -1,8 +1,8 @@
 
 const String BASE_URL = 'https://api.rawg.io/api/';
 
-const String GENRE_ENDPOINT = 'genres';
-const String GAMES_ENDPOINT = 'games';
-const String PUBLISHERS_ENDPOINT = 'publishers';
-const String DEVELOPERS_ENDPOINT = 'developers';
-const String PLATFORMS_ENDPOINT = 'platforms?ordering=year_start&page=1';
+const String GENRE_ENDPOINT =  BASE_URL + 'genres';
+const String GAMES_ENDPOINT = BASE_URL + 'games';
+const String PUBLISHERS_ENDPOINT = BASE_URL + 'publishers';
+const String DEVELOPERS_ENDPOINT = BASE_URL + 'developers';
+const String PLATFORMS_ENDPOINT =  BASE_URL + 'platforms?ordering=year_start&page=1';
--- lib/datasources/api/rawg_api.dart
+++ lib/datasources/api/rawg_api.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:game_app/datasources/api/api_utils.dart';
+import 'package:game_app/datasources/repo/z_repo.dart';
 import 'package:game_app/domain/models/zmodels.dart';
 import 'package:http/http.dart';
 
@@ -29,19 +30,19 @@ Future<Response> getGenres() async {
   );
 }
 
-Future<GamesModel> getAnticipatedService() async {
+Future<GenreModel> getGenresService() async {
 
   try{
 
     Response response =
-      await get(
-        GAMES_ENDPOINT + "?dates=${getCurrentYear()}-06-01,${getNextYear()}-06-01&ordering=-added&page=1",
-        headers: _userAgentHeader,
-      );
+    await get(
+      GENRE_ENDPOINT,
+      headers: _userAgentHeader,
+    );
 
     if(response.statusCode == 200 || response.statusCode == 201){
 
-      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      GenreModel gameModel = GenreModel.fromJson(json.decode(response.body));
       return gameModel;
 
     }else{
@@ -59,9 +60,52 @@ Future<Response> getGames(String genreString) async{
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<GamesModel> getYourGamesService() async {
+
+  try{
+
+    String genreString = await getGenreString();
+
+    Response response = await get(GAMES_ENDPOINT + '?genres=$genreString&page=1', headers: _userAgentHeader);
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getGamesFromDevelopers(String developers) async{
   return get("https://api.rawg.io/api/games?developers=$developers&page=1",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
+}
+
+Future<GamesModel> getYourGamesFromDevelopersService({String developers}) async {
+
+  try{
+
+    Response response = await get(GAMES_ENDPOINT + '?developers=$developers&page=1', headers: _userAgentHeader);
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
 }
 
 Future<Response> getGamesFromPublishers(String publishers) async{
@@ -69,9 +113,49 @@ Future<Response> getGamesFromPublishers(String publishers) async{
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<GamesModel> getYourGamesFromPublishersService({String publishers}) async {
+
+  try{
+
+    Response response = await get(GAMES_ENDPOINT + '?publishers=$publishers&page=1', headers: _userAgentHeader);
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getGamesFromPlatform(String platforms) async{
   return get("https://api.rawg.io/api/games?platforms=$platforms&page=1",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
+}
+
+Future<GamesModel> getYourGamesFromPlatformService({String platform}) async {
+
+  try{
+
+    Response response = await get(GAMES_ENDPOINT + '?platforms=$platform&page=1', headers: _userAgentHeader);
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
 }
 
 Future<Response> getGamesFromSearch(String query) async{
@@ -79,9 +163,55 @@ Future<Response> getGamesFromSearch(String query) async{
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<GamesModel> getYourGamesFromSearchService({String query}) async {
+
+  try{
+
+    Response response = await get(GAMES_ENDPOINT + '?search=$query&page=1', headers: _userAgentHeader);
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getGameDetail(int id) async{
   return get("https://api.rawg.io/api/games/$id",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
+
+}
+
+Future<GameDetailModel> getGameDetailsService({int id}) async {
+
+  try{
+
+    Response response =
+    await get(
+      GAMES_ENDPOINT + "/$id",
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GameDetailModel gameModel = GameDetailModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
 }
 
 Future<Response> getGameScreenshots(String slug) async{
@@ -89,9 +219,59 @@ Future<Response> getGameScreenshots(String slug) async{
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<ScreenshotsModel> getGameScreenshotsService({String slug}) async {
+
+  try{
+
+    Response response =
+    await get(
+      GAMES_ENDPOINT + "/$slug/screenshots",
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      ScreenshotsModel gameModel = ScreenshotsModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getGameTrailer(String slug) async{
   return get("https://api.rawg.io/api/games/$slug/movies",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
+}
+
+Future<TrailersModel> getGameTrailersService({String slug}) async {
+
+  try{
+
+    Response response =
+    await get(
+      GAMES_ENDPOINT + "/$slug/movies",
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      TrailersModel gameModel = TrailersModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
 }
 
 Future<Response> getGameAchievement(int id) async{
@@ -99,14 +279,90 @@ Future<Response> getGameAchievement(int id) async{
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<AchievementModel> getGameAchievementsService({int id}) async {
+
+  try{
+
+    Response response =
+    await get(
+      GAMES_ENDPOINT + "/$id/achievements",
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      AchievementModel gameModel = AchievementModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getPlatforms() async{
   return get("https://api.rawg.io/api/platforms?ordering=year_start&page=1",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<PlatformModel> getPlatformsService() async {
+
+  try{
+
+    Response response =
+    await get(
+      PLATFORMS_ENDPOINT,
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      PlatformModel gameModel = PlatformModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getPublishers() async{
   return get("https://api.rawg.io/api/publishers",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
+
+}
+
+Future<PublishersModel> getPublishersService() async {
+
+  try{
+
+    Response response =
+    await get(
+      PUBLISHERS_ENDPOINT,
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      PublishersModel gameModel = PublishersModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
 }
 
 Future<Response> getPopular() async{
@@ -114,14 +370,89 @@ Future<Response> getPopular() async{
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<GamesModel> getPopularService() async {
+
+  try{
+
+    Response response =
+    await get(
+      GAMES_ENDPOINT + "?dates=${getPastYear()}-06-01,${getCurrentYear()}-06-01&ordering=-added&page=1",
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getAnticipated() async{
   return get("https://api.rawg.io/api/games?dates=${getCurrentYear()}-06-01,${getNextYear()}-06-01&ordering=-added&page=1",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
 }
 
+Future<GamesModel> getAnticipatedService() async {
+
+  try{
+
+    Response response =
+    await get(
+      GAMES_ENDPOINT + "?dates=${getCurrentYear()}-06-01,${getNextYear()}-06-01&ordering=-added&page=1",
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      GamesModel gameModel = GamesModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
+}
+
 Future<Response> getDevelopers() async{
   return get("https://api.rawg.io/api/developers",
       headers: {HttpHeaders.userAgentHeader : "Eccentric Catalog"});
+}
+
+Future<PublishersModel> getDevelopersService() async {
+
+  try{
+
+    Response response =
+    await get(
+      DEVELOPERS_ENDPOINT,
+      headers: _userAgentHeader,
+    );
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+
+      PublishersModel gameModel = PublishersModel.fromJson(json.decode(response.body));
+      return gameModel;
+
+    }else{
+      throw Exception("${response.statusCode}, ${response.body}");
+    }
+
+  }catch(e){
+    throw Exception("$e");
+  }
+
 }
 
 //Game id: 36755
