16172cafe385d6f6d73ead72b57133b2e139f9d7
the-Jinxist <nerosilva522@gmail.com>
2021-02-08 19:03:35 +0100

    Added popular bloc skeleton, will flesh it out later

---
+++ lib/presentation/bloc/popular_bloc/z_popular_bloc.dart
@@ -0,0 +1,3 @@
+export 'package:game_app/presentation/bloc/popular_bloc/event/popular_event.dart';
+export 'package:game_app/presentation/bloc/popular_bloc/state/popular_state.dart';
+export 'package:game_app/presentation/bloc/popular_bloc/mapper/popular_bloc.dart';
