472570fe1a141f1c85459fa0ec22192f8da48a30
the-Jinxist <nerosilva522@gmail.com>
2021-02-15 22:57:46 +0100

    Formatted rawg_api. Start migrating remaining APIs

---
+++ lib/presentation/bloc/games_via_developers_bloc/z_games_via_developers_bloc.dart
@@ -0,0 +1,3 @@
+export 'package:game_app/presentation/bloc/games_via_developers_bloc/event/games_via_developers_event.dart';
+export 'package:game_app/presentation/bloc/games_via_developers_bloc/state/games_via_developers_state.dart';
+export 'package:game_app/presentation/bloc/games_via_developers_bloc/mapper/games_via_developers_bloc.dart';
---
+++ lib/presentation/bloc/games_via_publishers_bloc/z_games_via_publishers_bloc.dart
@@ -0,0 +1,3 @@
+export 'package:game_app/presentation/bloc/games_via_publishers_bloc/event/games_via_publishers_event.dart';
+export 'package:game_app/presentation/bloc/games_via_publishers_bloc/state/games_via_publishers_state.dart';
+export 'package:game_app/presentation/bloc/games_via_publishers_bloc/mapper/games_via_publishers_bloc.dart';
