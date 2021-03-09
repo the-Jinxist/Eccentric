8ad9c27f3d9cc530313af4af3c1888fea1d24a09
the-Jinxist <nerosilva522@gmail.com>
2021-03-09 15:50:58 +0100

    Moved all industry screens state management to bloc

--- lib/presentation/pages/industry/all_publishers_page.dart
+++ lib/presentation/pages/industry/all_publishers_page.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_app/datasources/api/rawg_api.dart' as api;
 import 'package:game_app/domain/models/publishers_model.dart';
 import 'package:game_app/domain/utils/size_config.dart';
+import 'package:game_app/presentation/bloc/z_bloc.dart';
 import 'package:game_app/presentation/pages/category/publishers_page.dart';
 import 'package:game_app/presentation/view/publisher_view.dart';
 import 'package:game_app/presentation/widgets/texts.dart';
@@ -15,14 +17,11 @@ class AllPublishersPage extends StatefulWidget {
 
 class _AllPublishersPageState extends State<AllPublishersPage> {
 
-  Future publishersFuture;
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
-    publishersFuture = _getPublishers();
+    BlocProvider.of<PublishersBloc>(context).add(LoadPublishers());
   }
 
   @override
@@ -40,80 +39,66 @@ class _AllPublishersPageState extends State<AllPublishersPage> {
           ],
         ),
       ), preferredSize: Size.fromHeight(100)),
-      body: FutureBuilder(
-        future: publishersFuture,
-        builder: (context, snapshot){
-          if(snapshot.connectionState != ConnectionState.done){
-            return Container(
-              height: SizeConfig.screenHeightDp,
-              width: SizeConfig.screenWidthDp,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          } else if(snapshot.hasData){
-            var publisherModel = snapshot.data as PublishersModel;
+      body: Column(
+        children: [
+          BlocBuilder<PublishersBloc, PublishersState>(
+              builder: (BuildContext context, PublishersState state) {
+                if (state is PublishersLoadInProgress) {
+                  return Container(
+                    height: SizeConfig.screenHeightDp,
+                    width: SizeConfig.screenWidthDp,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else if (state is PublishersLoadSuccess) {
+                  return ListView.builder(
+                      padding: EdgeInsets.only(left: 10, right: 5),
+                      itemCount: state.publishers.results.length,
+                      itemBuilder: (context, position){
+                        var model = state.publishers.results[position];
+                        return InkWell(
+                            onTap: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => PublishersPage(model)));
+                            },
+                            child: Container(margin: EdgeInsets.only(bottom: 5, top: 5),child: PublisherView(model, "publishers"))
+                        );
+                      }
+                  );
+                } else if (state is PublishersLoadFailure) {
+                  return Container(
+                    padding: EdgeInsets.all(20),
+                    height: SizeConfig.screenHeightDp,
+                    width: SizeConfig.screenWidthDp,
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          NormalText(text: "Sorry an error occured", textAlign: TextAlign.center,),
+                          GestureDetector(
+                            onTap: (){
 
-            return ListView.builder(
-                padding: EdgeInsets.only(left: 10, right: 5),
-                itemCount: publisherModel.results.length,
-                itemBuilder: (context, position){
-                  var model = publisherModel.results[position];
-                  return InkWell(
-                      onTap: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => PublishersPage(model)));
-                      },
-                      child: Container(margin: EdgeInsets.only(bottom: 5, top: 5),child: PublisherView(model, "publishers"))
+                            },
+                            child: TitleText(text: "Reload", textColor: Colors.orange, fontSize: 25,),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container(
+                    height: SizeConfig.screenHeightDp,
+                    width: SizeConfig.screenWidthDp,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
                   );
                 }
-            );
-          }else if(snapshot.hasError){
-            return Container(
-              padding: EdgeInsets.all(20),
-              height: SizeConfig.screenHeightDp,
-              width: SizeConfig.screenWidthDp,
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    NormalText(text: "Sorry an error occured", textAlign: TextAlign.center,),
-                    GestureDetector(
-                      onTap: (){
-                        setState(() {
-                          publishersFuture = _getPublishers();
-                        });
-                      },
-                      child: TitleText(text: "Reload", textColor: Colors.orange, fontSize: 25,),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else{
-            return Container(
-              height: SizeConfig.screenHeightDp,
-              width: SizeConfig.screenWidthDp,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-        },
+              })
+        ],
       ),
     );
-  }
-
-  Future<PublishersModel>_getPublishers() async{
-    var response = await api.getPublishers();
-    if(response.statusCode == 200){
-      var model = PublishersModel.fromJson(json.decode(response.body));
-      return model;
-    }else{
-      print("All Publishers Page Error: ${response.statusCode}");
-      return null;
-    }
-
   }
 
 }
