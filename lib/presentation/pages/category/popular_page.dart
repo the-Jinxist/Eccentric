cb35a1b85e7ab717f3bb6d0e7432a190ca557aca
the-Jinxist <nerosilva522@gmail.com>
2021-03-09 15:39:15 +0100

    Moved all category screens state management to bloc

--- lib/presentation/pages/category/popular_page.dart
+++ lib/presentation/pages/category/popular_page.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_app/datasources/api/rawg_api.dart' as api;
 import 'package:game_app/domain/models/games_model.dart';
 import 'package:game_app/domain/utils/size_config.dart';
+import 'package:game_app/presentation/bloc/z_bloc.dart';
 import 'package:game_app/presentation/pages/details/game_details_page.dart';
 import 'package:game_app/presentation/view/game_view.dart';
 import 'package:game_app/presentation/widgets/texts.dart';
@@ -15,14 +17,13 @@ class PopularPage extends StatefulWidget {
 
 class _PopularPageState extends State<PopularPage> {
 
-  Future loadGamesFuture;
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
-    loadGamesFuture = getGames();
+    BlocProvider.of<PopularBloc>(context).add(
+        LoadPopular());
+
   }
 
   @override
@@ -45,112 +46,100 @@ class _PopularPageState extends State<PopularPage> {
       ),
         body: Builder(
             builder: (context){
-              return FutureBuilder(
-                future: loadGamesFuture,
-                builder: (context, snapshot){
-                  if(snapshot.connectionState != ConnectionState.done){
-                    return Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  } else if(snapshot.hasData){
-                    var gameModeL = snapshot.data as GamesModel;
+              return Column(
+                children: [
+                  BlocBuilder<PopularBloc, PopularState>(
+                    builder: (context, state){
+                      if(state is PopularLoadInProgress){
+                        return Container(
+                          height: SizeConfig.screenHeightDp,
+                          width: SizeConfig.screenWidthDp,
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        );
+                      }else if(state is PopularLoadSuccess){
 
-                    return ListView.builder(
-                        itemCount: gameModeL.results.length,
-                        itemBuilder: (context, position){
-                          var currentGame = gameModeL.results[position];
-                          return InkWell(child: GameView(result: gameModeL.results[position],
-                              onSavedTap: (string) {
+                        return ListView.builder(
+                            itemCount: state.games.results.length,
+                            itemBuilder: (context, position){
+                              var currentGame = state.games.results[position];
+                              return InkWell(child: GameView(result: state.games.results[position],
+                                onSavedTap: (string) {
 
-                                if (string == "Added") {
-                                  Scaffold.of(context).showSnackBar(
-                                      SnackBar(
-                                          backgroundColor: Colors.black,
-                                          content: NormalText(text: "Game added to favourite!")
-                                      )
-                                  );
-                                } else {
-                                  Scaffold.of(context).showSnackBar(
-                                      SnackBar(
-                                          backgroundColor: Colors.black,
-                                          content: NormalText(text: "Game removed from favourite!")
-                                      )
-                                  );
-                                }
-                              },),
-                            onTap: (){
-                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => GameDetailsPage(
-                                backgroundImage: currentGame.backgroundImage,
-                                id: currentGame.id,
-                                metacriticRating: currentGame.metacritic,
-                                name: currentGame.name,
-                                playTime: currentGame.playtime,
-                                rating: currentGame.rating,
-                                ratingsCount: currentGame.ratingsCount,
-                                ratingsTop: currentGame.ratingsTop,
-                                releaseDate: currentGame.released,
-                                slug: currentGame.slug,
-                                suggestionsCount: currentGame.suggestionsCount,
-                              )));
-                            },
+                                  if (string == "Added") {
+                                    Scaffold.of(context).showSnackBar(
+                                        SnackBar(
+                                            backgroundColor: Colors.black,
+                                            content: NormalText(text: "Game added to favourite!")
+                                        )
+                                    );
+                                  } else {
+                                    Scaffold.of(context).showSnackBar(
+                                        SnackBar(
+                                            backgroundColor: Colors.black,
+                                            content: NormalText(text: "Game removed from favourite!")
+                                        )
+                                    );
+                                  }
+                                },),
+                                onTap: (){
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => GameDetailsPage(
+                                    backgroundImage: currentGame.backgroundImage,
+                                    id: currentGame.id,
+                                    metacriticRating: currentGame.metacritic,
+                                    name: currentGame.name,
+                                    playTime: currentGame.playtime,
+                                    rating: currentGame.rating,
+                                    ratingsCount: currentGame.ratingsCount,
+                                    ratingsTop: currentGame.ratingsTop,
+                                    releaseDate: currentGame.released,
+                                    slug: currentGame.slug,
+                                    suggestionsCount: currentGame.suggestionsCount,
+                                  )));
+                                },
 
-                          );
-                        }
-                    );
-                  }else if(snapshot.hasError){
-                    return Container(
-                      padding: EdgeInsets.all(20),
-                      height: SizeConfig.screenHeightDp,
-                      width: SizeConfig.screenWidthDp,
-                      child: Center(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            NormalText(text: "Sorry an error occured", textAlign: TextAlign.center,),
-                            GestureDetector(
-                              onTap: (){
-                                setState(() {
-                                  loadGamesFuture = getGames();
-                                });
-                              },
-                              child: NormalText(text: "Reload", textColor: Colors.orange, fontSize: 25, ),
+                              );
+                            }
+                        );
+
+                      }else if(state is PopularLoadFailure){
+                        return Container(
+                          padding: EdgeInsets.all(20),
+                          height: SizeConfig.screenHeightDp,
+                          width: SizeConfig.screenWidthDp,
+                          child: Center(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                NormalText(text: "Sorry an error occured", textAlign: TextAlign.center,),
+                                GestureDetector(
+                                  onTap: (){
+                                    BlocProvider.of<PopularBloc>(context).add(
+                                        LoadPopular());
+                                  },
+                                  child: NormalText(text: "Reload", textColor: Colors.orange, fontSize: 25, ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                    );
-                  } else{
-                    return Container(
-                      height: SizeConfig.screenHeightDp,
-                      width: SizeConfig.screenWidthDp,
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  }
-                },
+                          ),
+                        );
+                      }else{
+                        return Container(
+                          height: SizeConfig.screenHeightDp,
+                          width: SizeConfig.screenWidthDp,
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        );
+                      }
+                    }
+                  ),
+                ],
               );
             }
         )
     );
-  }
-
-  Future<GamesModel> getGames() async {
-
-    var response  = await api.getPopular();
-
-    if (response.statusCode == 200){
-      var responseBody = json.decode(response.body);
-//      print("Platform Page: ${GamesModel.fromJson(responseBody).results[3].slug}");
-      return GamesModel.fromJson(responseBody);
-    }else{
-      print("Popular Page: ${response.statusCode}");
-      return null;
-    }
   }
 }
