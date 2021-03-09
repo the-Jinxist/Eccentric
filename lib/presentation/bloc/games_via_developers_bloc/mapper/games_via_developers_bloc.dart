804cbfc41c665e0afe518c156757e5535d84f894
the-Jinxist <nerosilva522@gmail.com>
2021-03-09 13:23:36 +0100

    Moved all APIs to bloc impl

--- lib/presentation/bloc/games_via_developers_bloc/mapper/games_via_developers_bloc.dart
+++ lib/presentation/bloc/games_via_developers_bloc/mapper/games_via_developers_bloc.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_app/datasources/api/rawg_api.dart';
 import 'package:game_app/domain/models/zmodels.dart';
-import 'package:game_app/presentation/bloc/developers_bloc/z_developers_bloc.dart';
 import 'package:game_app/presentation/bloc/games_via_developers_bloc/z_games_via_developers_bloc.dart';
-import 'package:game_app/presentation/bloc/publishers_bloc/z_publishers_bloc.dart';
 
 class GamesViaDevelopersBloc extends Bloc<GamesViaDevelopersEvent, GamesViaDevelopersState>{
 
