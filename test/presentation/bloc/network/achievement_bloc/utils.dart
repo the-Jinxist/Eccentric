c84de29d75c444987ca72d69f1bcd3edb443b78e
the-Jinxist <nerosilva522@gmail.com>
2021-05-06 15:00:03 +0100

    Removed useless statements

--- test/presentation/bloc/network/achievement_bloc/utils.dart
+++ test/presentation/bloc/network/achievement_bloc/utils.dart
@@ -1,16 +1,4 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/material.dart';
-import 'package:game_app/datasources/z_datasources.dart';
 import 'package:game_app/presentation/bloc/z_bloc.dart';
-import 'package:mockito/mockito.dart';
-
-const idleState = TypeMatcher<AchievementIdleState>();
-const loadingState =  TypeMatcher<AchievementLoadInProgress>();
-const successState = TypeMatcher<AchievementLoadSuccess>();
-const failureState = TypeMatcher<AchievementLoadFailure>();
-
-class MockDatabase extends Mock implements DatabaseHelper{
-
-}
 
 class MockAchievementBloc extends MockBloc<AchievementEvent, AchievementState> implements AchievementBloc{}
