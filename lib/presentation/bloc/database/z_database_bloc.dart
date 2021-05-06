a6ff2c1fb53d778fdb89ec4b3b90d3ea3a475a61
the-Jinxist <nerosilva522@gmail.com>
2021-05-06 15:09:21 +0100

    Removed useless files

--- lib/presentation/bloc/database/z_database_bloc.dart
+++
@@ -1,5 +0,0 @@
-export 'package:game_app/presentation/bloc/database/delete_game_bloc/z_delete_game_bloc.dart';
-export 'package:game_app/presentation/bloc/database/get_all_games_bloc/z_get_all_games_bloc.dart';
-export 'package:game_app/presentation/bloc/database/get_single_game_bloc/z_get_single_game_bloc.dart';
-export 'package:game_app/presentation/bloc/database/insert_single_game_bloc/z_insert_single_game_bloc.dart';
-export 'package:game_app/presentation/bloc/database/insert_games_bloc/z_insert_games_bloc.dart';
